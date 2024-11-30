a7f7e70f26bc553afae668f62552f3263e3396bd
Toby Liu <toby.liu@hulu.com>
2024-11-29 18:06:37 -0800

    Introduce ImageWrapper

---
+++ src/components/ImageWrapper/ImageWrapper.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+/**
+ * ImageWrapper is going to be a wrapper around <img /> tag.
+ * It will enforce a11y, standardized styling, and interactivity.
+ * It will also offer retry logic.
+ * And it will offer a loading state to display while image is loading. 
+ */
+
+export const ImageWrapper = () => {
+  return <div>Image Wrapper</div>;
+};
