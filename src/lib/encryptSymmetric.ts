6a3040135f60ea314fbeb9a7662f48249dbb0643
Toby Liu <ybot1122@gmail.com>
2025-01-16 18:19:44 -0800

    fix encryptSymmetric

--- src/lib/encryptSymmetric.ts
+++ src/lib/encryptSymmetric.ts
@@ -1,5 +1,7 @@
 // https://medium.com/@tony.infisical/guide-to-nodes-crypto-module-for-encryption-decryption-65c077176980
 
+// Generate 32 len random string: https://www.random.org/strings/
+
 import crypto from "node:crypto";
 
 export const encryptSymmetric = ({
@@ -10,27 +12,10 @@ export const encryptSymmetric = ({
   plaintext: string;
 }): string => {
   const iv = crypto.randomBytes(12).toString("base64");
-  const cipher = crypto.createCipheriv(
-    "aes-256-gcm",
-    Buffer.from(key, "base64"),
-    Buffer.from(iv, "base64"),
-  );
+  const cipher = crypto.createCipheriv("aes-256-gcm", key, iv);
   let ciphertext = cipher.update(plaintext, "utf8", "base64");
   ciphertext += cipher.final("base64");
   const tag = cipher.getAuthTag().toString("base64");
 
   return `${ciphertext}.${tag}.${iv}`;
 };
-
-/*
-
-How to generate a key:
-
-const { generateKey } = require("node:crypto");
-
-generateKey("hmac", { length: 128 }, (err, key) => {
-  if (err) throw err;
-  console.log(key.export().toString("hex")); // 46e..........620
-});
-
-*/
