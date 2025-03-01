df7a80ce39d96fb99ef996e8b17e0d1222d26923
Toby Liu <ybot1122@gmail.com>
2025-03-01 12:49:52 -0800

    Docs website fix (#43)
    - Fixes base path for assets

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,9 +4,9 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["**/dist/**"] },
+  { ignores: ["**/dist/**", "**/docs/build/**"] },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
+  ...tseslint.configs.stylistic
 );
