a3cd2d6911fc46541cd135e4df03e21dff3b258b
Toby Liu <ybot1122@gmail.com>
2025-06-18 09:23:51 -0700

    Github link

--- docs/components/SdkSection.tsx
+++ docs/components/SdkSection.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export const SdkSection = ({
   id,
   title,
@@ -7,10 +9,16 @@ export const SdkSection = ({
   title: string;
   children: React.ReactNode;
 }) => {
+  const [isCollapsed, setIsCollapsed] = useState(true);
   return (
     <section className="mb-8" id={id}>
-      <h2 className="text-2xl font-bold">{title}</h2>
-      <div>{children}</div>
+      <h2 className="text-2xl font-bold">
+        {title}{" "}
+        <button onClick={() => setIsCollapsed((t) => !t)}>
+          {isCollapsed ? "Show More" : "Hide"}
+        </button>
+      </h2>
+      {!isCollapsed && <div>{children}</div>}
     </section>
   );
 };
